04d343147c7abcf69ece448b47b1d4ff49c3ee35
Rahul Sharma <rahulnogravity@gmail.com>
2018-06-24 20:43:24 +0530

    Initial commit

---
+++ MTWiki-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  MTWiki-Bridging-Header.h
+//  MTWiki
+//
+//  Created by Rahul Sharma on 23/06/18.
+//  Copyright © 2018 Rahul Sharma. All rights reserved.
+//
+
+#ifndef MTWiki_Bridging_Header_h
+#define MTWiki_Bridging_Header_h
+
+#import <UIKit/UIKit.h>
+#import "MagicalRecord.h"
+#import "UIImageView+WebCache.h"
+
+#endif /* MTWiki_Bridging_Header_h */
